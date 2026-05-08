f532e8e6094d9e812065f7a8792343eae8e099a8
unknown <supreme.ai.facts@gmail.com>
2026-05-08 23:32:42 +0800

    set render4

--- functions/auth/status.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestGet() {
-  return new Response(JSON.stringify({
-    authenticated: false,
-    message: 'Auth status endpoint running'
-  }), {
-    status: 200,
-    headers: { 'Content-Type': 'application/json' }
-  });
-}
